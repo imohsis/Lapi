b772134c9163f413ceb0dbd9c9e4b23c8bb695e8
imoh <imohsis@gmail.com>
2018-04-17 14:56:52 +0100

    Add seeders for News and Reviews

--- database/factories/ModelNewsFactory.php
+++ database/factories/ModelNewsFactory.php
@@ -4,6 +4,17 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\News::class, function (Faker $faker) {
     return [
-        //
+        
+        
+        'title' => $faker->word,
+        'subtitle' => $faker->word,
+        'main_image' => $faker->imageUrl($width = 640, $height = 480),
+        'body' => $faker->paragraph,
+        'images' => $faker->imageUrl($width = 640, $height = 480),
+        'categories' => $faker->word,
+        'tags' => $faker->word
+
+
+
     ];
 });
--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -1,9 +1,21 @@
 <?php
 
+use App\Model\News;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+
+       'news_id' => function ()
+       {
+       	  return News::all()->random();
+       },
+
+       'user' => $faker->name,
+       'review' => $faker->paragraph,
+       'star' => $faker->numberBetween(0,5)
+
+
+
     ];
 });
