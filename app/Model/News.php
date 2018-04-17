a542662e6af954d18a4b7cb47b27d9be1f9eba25
imoh <imohsis@gmail.com>
2018-04-17 15:22:53 +0100

    Added my database relationship between news and reviews

--- app/Model/News.php
+++ app/Model/News.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    //
+    public function reviews(){
+
+    	return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,14 @@
 
 namespace App\Model;
 
+use App\Model\News;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function news(){
+
+    	return $this->belongsTo(News::class);
+    }
 }
