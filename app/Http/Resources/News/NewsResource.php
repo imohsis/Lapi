50248a20b397d1586b9341b1494186f923f33881
imoh <imohsis@gmail.com>
2018-04-18 17:19:58 +0100

    Manipulate the resource in other to send calculated data to the view

--- app/Http/Resources/News/NewsResource.php
+++ app/Http/Resources/News/NewsResource.php
@@ -15,7 +15,7 @@ class NewsResource extends JsonResource
     public function toArray($request)
     {
         return[
-
+// you can add as many field here as you want to manipulate
        'title' => $this->title,
        'subtitle' => $this->subtitle,
        'main_image' => $this->main_image,
@@ -26,6 +26,14 @@ class NewsResource extends JsonResource
        'like'=> $this->like,
        'dislike'=> $this->dislike,
        'created_at'=> $this->created_at,
+       'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No Ratings yet',
+
+
+         'href' => [
+
+                ' reviews' => route('reviews.index', $this->id)
+
+                 ]
 
 
 
